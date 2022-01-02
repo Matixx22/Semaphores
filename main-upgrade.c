917f1cf25b611dc148f926c8cf3b451a999e1001
Mateusz <mateuszsuchocki7@gmail.com>
2022-01-02 10:55:32 -0800

    Prepare upgrade for testing

--- main-upgrade.c
+++ main-upgrade.c
@@ -8,21 +8,31 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <signal.h>
 #include "queue.h"
 
-#define Q_CAP 100
-#define Q_NUM 10 
+#define Q_CAP 5
+#define Q_NUM 2 
+#define P_NUM 2
+#define C_NUM 2
 
-pthread_t prod_thread1, prod_thread2, cons_thread1, cons_thread2;
+pthread_t prod_t[P_NUM];
+pthread_t cons_t[C_NUM];
 
 struct Queue* queues[Q_NUM];
 
-void prod_alarm() {
+void sig_handler(int sig) {
+	printf("Consumer id %d received special message. Quiting...\n", gettid());
+	pthread_exit(NULL);
+}
+
+void prod_alarm(int tid) {
 	int i;
 	for (i = 0; i < Q_NUM; ++i) {
-		enqueue(queues[i], INT_MAX);
+		printf("Producent id %d proudced special message to queue %d\n", tid, i);
+		enqueue(queues[i], INT_MAX, tid);
 	}
-	printf("Alarm produced\n");
+	//printf("Alarm produced\n");
 }
 
 void producer(int item, int tid) {
@@ -35,9 +45,9 @@ void producer(int item, int tid) {
 		sleep(1);	
 	}
 
-	if (front(queues[id]) == INT_MAX) return;
+	if (front(queues[id]) == INT_MAX) pthread_exit(NULL);
 
-	enqueue(queues[id], item);
+	enqueue(queues[id], item, tid);
 	printf("Producer id %d produced item: %d to queue: %d\n", tid, item, id);
 }
 
@@ -51,12 +61,13 @@ int consumer(int tid) {
 		id = (id+1) % Q_NUM;
 		sleep(1);
 	}
-	item = dequeue(queues[id]);
+
+	item = dequeue(queues[id], tid);
 	if (item == INT_MAX) {
-		printf("Alarm received by consumer %d from queue %d. Clearing queues...\n", tid, id);	
+		//printf("Alarm received by consumer %d from queue %d. Clearing queues...\n", tid, id);	
 		return(INT_MAX);
 	}
-
+	
 	printf("Consumer id %d consumed item: %d from queue: %d\n", tid, item, id);
 
 	return(item);
@@ -64,34 +75,38 @@ int consumer(int tid) {
 
 void* prod1() {
 	int i = 0, j, k;
-	while (i < 10) {
-		producer(i, pthread_self());
+	while (i < 7) {
+		producer(i, gettid());
 		++i;
-		for (j = 0; j < 10; ++j) { for (k = 0; k < 9999999; ++k) {} }
+		for (j = 0; j < 1; ++j) { for (k = 0; k < 9999999; ++k) {} }
 	}
 }
 
 void* prod2() {
 	int i = 0, j, k;
-	while (i < 15) {
-		producer(i, pthread_self());
+	while (i < 3) {
+		producer(i, gettid());
 		++i;
-		for (j = 0; j < 20; ++j) { for (k = 0; k < 9999999; ++k) {} }
+		for (j = 0; j < 5; ++j) { for (k = 0; k < 9999999; ++k) {} }
 	}
-	prod_alarm();
+	//prod_alarm(gettid());
 }
 
 void* cons1() {
 	int item;
 	int i = 0, j, k;
 
-	while (i < 100) {
-		item = consumer(pthread_self());
+	while (i < 10) {
+		item = consumer(gettid());
 		if (item == INT_MAX) {
-			pthread_exit(NULL);
+			// send signal to every consumer thread
+			for (j = 0; j < C_NUM; ++j) {
+				pthread_kill(cons_t[j], SIGUSR1);
+			}
+			exit(0);
 		}
 		++i;
-		for (j = 0; j < 1; ++j) { for (k = 0; k < 9999999; ++k) {} }
+		for (j = 0; j < 15; ++j) { for (k = 0; k < 9999999; ++k) {} }
 	}
 }
 
@@ -99,35 +114,41 @@ void* cons2() {
 	int item;
 	int i = 0, j, k;
 
-	while (i < 40) {
-		item = consumer(pthread_self());
+	while (i < 10) {
+		item = consumer(gettid());
 		if (item == INT_MAX) {
-			pthread_exit(NULL);	
+			// send signal to every consumer thread
+			for (j = 0; j < C_NUM; ++j) {
+				pthread_kill(cons_t[j], SIGUSR1);
+			}
+			exit(0);	
 		}
 		++i;
-		for (j = 0; j < 3; ++j) { for (k = 0; k < 9999999; ++k) {} }
+		for (j = 0; j < 20; ++j) { for (k = 0; k < 9999999; ++k) {} }
 	}
 }
 
 int main(int argc, char** argv) {
 	int i;
 	int item;
+
 	srand(time(NULL));	
+	signal(SIGUSR1, sig_handler);
 
 	// init queues
 	for (i = 0; i < Q_NUM; ++i) {
 		queues[i] = createQueue(Q_CAP, i);
 	}
 
-	pthread_create(&prod_thread1, NULL, prod1, NULL);
-	pthread_create(&prod_thread2, NULL, prod2, NULL);
-	pthread_create(&cons_thread1, NULL, cons1, NULL);
-	pthread_create(&cons_thread2, NULL, cons2, NULL);
+	pthread_create(&prod_t[0], NULL, prod1, NULL);
+	pthread_create(&prod_t[1], NULL, prod2, NULL);
+	pthread_create(&cons_t[0], NULL, cons1, NULL);
+	pthread_create(&cons_t[1], NULL, cons2, NULL);
 
-	pthread_join(prod_thread1, NULL);
-	pthread_join(prod_thread2, NULL);
-	pthread_join(cons_thread1, NULL);
-	pthread_join(cons_thread2, NULL);
+	pthread_join(prod_t[0], NULL);
+	pthread_join(prod_t[1], NULL);
+	pthread_join(cons_t[0], NULL);
+	pthread_join(cons_t[1], NULL);
 
 	return 0;
 }
